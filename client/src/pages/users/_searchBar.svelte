9f9b875e27f5462128031329b13bc7311cdbef7f
= <=>
2024-04-04 19:32:24 -0300

    fixed warning, responsive and persistence issue (client).

--- client/src/pages/users/_searchBar.svelte
+++ client/src/pages/users/_searchBar.svelte
@@ -19,40 +19,38 @@
   };
 </script>
 
-<div class="mt-2 h-96 w-full overflow-y-auto lg:w-1/4">
-  <div class="relative">
-    <input
-      type="text"
-      name="search"
-      class="h-10 w-full rounded-md border px-4 text-right placeholder:font-medium placeholder:text-black dark:border-neutral-900"
-      placeholder="Search for an user"
-      on:input={search}
-      bind:value={form.search}
-    />
-    <div class="absolute left-2 top-2.5">
-      <span>
-        <MagnifierGlass />
-      </span>
-    </div>
+<div class="relative">
+  <input
+    type="text"
+    name="search"
+    class="h-10 w-full rounded-md border px-4 text-right placeholder:font-medium placeholder:text-black dark:border-neutral-900"
+    placeholder="Search for an user"
+    on:input={search}
+    bind:value={form.search}
+  />
+  <div class="absolute left-2 top-2.5">
+    <span>
+      <MagnifierGlass />
+    </span>
   </div>
-  {#each body as item}
-    <a
-      class="my-2 flex h-20 items-center justify-between rounded-md border bg-gray-100 px-4 py-2 dark:border-neutral-900 dark:bg-neutral-700 dark:text-white"
-      href={`/users/${item.id}`}
-    >
-      <span class="">
-        <img
-          width={32}
-          height={32}
-          class="rounded-full border border-neutral-900"
-          src={"/placeholder.png"}
-          alt={`user ${item.id}`}
-          loading={"eager"}
-        />{" "}
-      </span>
-      <span class="ml-2 line-clamp-1 dark:text-white">
-        {item.name}
-      </span>
-    </a>
-  {/each}
 </div>
+{#each body as item}
+  <a
+    class="my-2 flex h-20 items-center justify-between rounded-md border bg-gray-100 px-4 py-2 dark:border-neutral-900 dark:bg-neutral-700 dark:text-white"
+    href={`/users/${item.id}`}
+  >
+    <span class="">
+      <img
+        width={32}
+        height={32}
+        class="rounded-full border border-neutral-900"
+        src={"/placeholder.png"}
+        alt={`user ${item.id}`}
+        loading={"eager"}
+      />{" "}
+    </span>
+    <span class="ml-2 line-clamp-1 dark:text-white">
+      {item.name}
+    </span>
+  </a>
+{/each}
