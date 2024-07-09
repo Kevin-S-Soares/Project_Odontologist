4f5300caf253d14e4dcaa2315c93268cec0fdb6c
= <=>
2024-07-08 21:13:33 -0300

    fixed responsivity issues (client).

--- client/src/components/panel/formPanel.svelte
+++ client/src/components/panel/formPanel.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="w-11/12 min-w-min rounded {bg_color} p-4 text-lg sm:w-1/2 md:w-6/12 lg:w-3/12"
+  class="w-11/12 rounded {bg_color} p-4 text-lg sm:w-1/2 md:w-6/12 lg:w-3/12"
 >
   <slot />
 </div>
